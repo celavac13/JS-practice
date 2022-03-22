ce579c57deb2e1bcab3c307ed3b5fbe985ba8e9a
stefan stevanovic <stefan713@live.com>
2022-03-22 11:16:38 +0100

    added svg

--- form1/script.js
+++ form1/script.js
@@ -8,10 +8,10 @@ const [fName, lName, email, password] = inputElements;
 
 const checkForErrors = function () {
     let errors = [];
-    if (fName.value === "") errors.push([fName, "fName"]);
-    if (lName.value === "") errors.push([lName, "lName"]);
-    if (!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) errors.push([email, "email"]);
-    if (password.value === "") errors.push([password, "password"]);
+    if (fName.value === "") errors.push([fName, "fName", "svgfName"]);
+    if (lName.value === "") errors.push([lName, "lName", "svglName"]);
+    if (!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) errors.push([email, "email", "svgEmail"]);
+    if (password.value === "") errors.push([password, "password", "svgPassword"]);
     return errors;
 };
 
@@ -28,9 +28,11 @@ btnForm.addEventListener("click", (e) => {
     if (errors.length === 0) {
         form.submit();
     } else {
+        console.log(errors);
         errors.forEach(el => {
             el[0].classList.add("content__apply__form__input__error");
             document.getElementById(el[1]).classList.remove("hidden");
+            document.getElementById(el[2]).classList.remove("hidden");
         });
     };
 });
@@ -40,5 +42,6 @@ inputElements.forEach(el => {
     el.addEventListener("focus", () => {
         inputElements.forEach(el => el.classList.remove("content__apply__form__input__error"));
         document.querySelectorAll("label").forEach(el => el.classList.add("hidden"));
+        document.querySelectorAll("svg").forEach(el => el.classList.add("hidden"));
     })
 })
